c0a63108c74865cf78a533c56f926a9eca19603c
김가영 <96832560+UhDgayeong@users.noreply.github.com>
2023-02-13 02:20:37 +0900

    [level 2] Title: 방문 길이, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,25 @@
+def solution(dirs):
+    arr = [[0] * 10 for _ in range(10)]
+    road = []
+    pos_x = 5
+    pos_y = 5
+    
+    for d in dirs:
+        tmp = [pos_x, -1, pos_y]
+        if d == "U" and pos_y > 0:
+            pos_y -= 1
+            tmp.insert(2, pos_y)
+        elif d == "D" and pos_y < 10:
+            pos_y += 1
+            tmp.append(pos_y)
+        elif d == "L" and pos_x > 0:
+            pos_x -= 1
+            tmp.insert(0, pos_x)
+        elif d == "R" and pos_x < 10:
+            pos_x += 1
+            tmp.insert(1, pos_x)
+        else:
+            continue
+        road.append(tuple(tmp))
+
+    return len(set(road))
