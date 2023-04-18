98e86a7ac1cb8187ec217a4907dab85da91b7d0a
김가영 <96832560+UhDgayeong@users.noreply.github.com>
2023-04-19 00:10:38 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 717.85 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -2,11 +2,9 @@ import math
 
 def solution(r1, r2):
     answer = 0
+    
     for y in range(1, r2):
-        if y <= r1:
-            x_min = math.ceil(math.sqrt((r1**2 - y**2)))
-        else:
-            x_min = 0
+        x_min = math.ceil(math.sqrt((r1**2 - y**2))) if y <= r1 else 0
         x_max = math.floor(math.sqrt((r2**2 - y**2)))
         answer += x_max - x_min + 1
         
