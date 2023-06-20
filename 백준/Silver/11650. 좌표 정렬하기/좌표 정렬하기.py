b923d50aa11a6297f614a5368f9146ebdfc69a54
김가영 <96832560+UhDgayeong@users.noreply.github.com>
2023-06-21 01:45:44 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 428 ms, Memory: 147160 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+import sys
+
+n = int(sys.stdin.readline())
+dots = defaultdict(list)
+
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    dots[x].append(y)
+
+dots = dict(sorted(dots.items(), key = lambda x : x[0]))
+
+for x in dots:
+    dots[x].sort()
+    for y in dots[x]:
+        print(str(x), str(y))
